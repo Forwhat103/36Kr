931b31843f72d7369db82a13692d98610b7636d6
朱小明 <942587133@qq.com>
2016-05-09 11:35:17 +0800

    第一次上传

--- 36Kr/app/src/main/java/com/zhuxiaoming/a36kr/MainActivity.java
+++ 36Kr/app/src/main/java/com/zhuxiaoming/a36kr/MainActivity.java
@@ -3,6 +3,9 @@ package com.zhuxiaoming.a36kr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这是主页
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
